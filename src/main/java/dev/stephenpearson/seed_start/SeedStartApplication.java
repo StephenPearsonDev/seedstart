852716109e7239a18e9f54d82bcdd080a9832f01
Stephen <pearsonsw@gmail.com>
2024-09-13 23:54:00 +0200

    Initial commit

---
+++ src/main/java/dev/stephenpearson/seed_start/SeedStartApplication.java
@@ -0,0 +1,13 @@
+package dev.stephenpearson.seed_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedStartApplication.class, args);
+	}
+
+}
